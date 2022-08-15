0426fdafc707221c552b0333b80d6bf51c56d8e9
Habib2711 <matias.habib@gmail.com>
2022-08-15 16:12:04 -0300

    back end creado

---
+++ Proyecto_MHBN/Backend/src/test/java/com/portfolio/mhbn/MhbnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mhbn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhbnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
